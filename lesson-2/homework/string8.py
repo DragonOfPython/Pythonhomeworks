1df0d4955f65394897795eba4696945087635adb
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2025-02-09 17:03:09 +0500

    homework

---
+++ lesson-2/homework/string8.py
@@ -0,0 +1,8 @@
+""" Write a program that asks the user for a string and 
+prints the first and last characters of the string. """
+
+a = str(input("please, enter the one string text: "))
+b = a[0]
+c = a[-1]
+
+print(f"The first and last characters of {a} string are {b} and {c}")
---
+++ lesson-2/homework/string9.py
@@ -0,0 +1,6 @@
+""" Ask the user for a string and 
+print the reversed version of it.  """
+
+a = str(input("Please, enter some text: "))
+a = a[::-1]
+print(a)
