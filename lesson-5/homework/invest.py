76735b6aedb5b433148f60008c35c034ba5d20e7
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2025-02-15 21:01:41 +0500

    invest.py

--- lesson-5/homework/invest.py
+++ lesson-5/homework/invest.py
@@ -1,6 +1,6 @@
 def invest(amount, rate, years):
-    i = 1
-    for i  in range(years):
+    years = years + 1
+    for i  in range(1, years):
         foyda = amount + rate * amount  # The formula to calculate the foyda
         amount = foyda # New amount will be equal to foyda of each year
         print(f"year {i}: ${round(foyda, 2)}") # print foyda
