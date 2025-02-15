9f4662700d7b543d47b643e87a8ea03c1dd33a9d
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2025-02-15 20:41:47 +0500

    temperature.py

--- lesson-5/homework/temperature.py
+++ lesson-5/homework/temperature.py
@@ -1,6 +1,6 @@
 def convert_far_to_cel():
     Fahrenheit = int(input("Enter a temperature in degrees F: "))
-    Celsius = (Fahrenheit - 32) * 5/9
+    Celsius = (Fahrenheit - 32) * 5/9 # Converting Fahrenheit to Celsius
     Celsius = round(Celsius, 2)
 
     print(f"{Fahrenheit} degree F = {Celsius} degree C")
@@ -9,7 +9,7 @@ convert_far_to_cel()
 
 def convert_cel_to_far():
     Celsius = int(input("Enter a temperature in degrees C: "))
-    Fahrenheit = Celsius * 9/5 + 32
+    Fahrenheit = Celsius * 9/5 + 32 # Converting Celsius to Fahrenheit
     Fahrenheit = round(Fahrenheit, 2)
 
     print(f"{Celsius} degree C = {Fahrenheit} degree F")
